3531f5cb2aa201d0a9df29ef7faab808842a91d9
Shinya Miyoshi <shinyamiyoshiac@gmail.com>
2024-05-12 12:23:12 +0900

    healthcheckエンドポイント作成

--- api/src/cmd/main.go
+++ api/src/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 
 	config "github.com/shinya-ac/TodoAPI/configs"
@@ -12,4 +13,14 @@ func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	logger.Info("hello slog", "name", "slog")
 	fmt.Println(config.Config.Host)
+
+	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "ok")
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("サーバー起動エラー：", err)
+		return
+	}
 }
